35d68efb54d2946cdb8f78bbacd3f28a54224f80
User <user@Users-MacBook-Pro.local>
2021-06-21 12:46:17 -0700

    init

---
+++ app/Http/Controllers/Bet1xController.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PDO;
+use mysqli;
+
+class Bet1xController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function makeDeposit(){
+        $body = json_decode(@file_get_contents("php://input"));
+        // var_dump($body->ResponseCode);
+        // die();
+
+        // if(isset($_POST['ResponseCode']) && $_POST['Message'] && $_POST['Amount'] && $_POST['TraceId'] && $_POST['PhoneNumber'] && $_POST['Refrence'] && $_POST['InstitutionCode'] && $_POST['TransactionId'] && $_POST['TraceId']){
+        if($body){
+
+            $ResponseCode = $body->ResponseCode;
+            $msisdn = "";
+            $Message = $body->Message;
+            $amount = $body->Amount;
+            $TraceId = $body->TraceId;
+            $PhoneNumber = $body->PhoneNumber;
+            $Refrence = $body->Refrence;
+            $InstitutionCode = $body->InstitutionCode;
+            $TransactionId = $body->TransactionId;
+            // if($body->msisdn){
+            //     $msisdn = $body->msisdn;
+            // }
+
+            $getUser = $this->getTransactionBet($Refrence);
+            // var_dump($getUser); exit;
+            if($getUser){
+                $msisdn = $getUser->msisdn;
+                $amount = $getUser->amount;
+            }else{
+                return response(['response'=>"Some error occurred", 200]);
+            }
+            
+
+            if ($ResponseCode == 00){
+                // var_dump("here"); exit;
+                 //API to send payment request to 1xbet
+                $url = 'https://aux-one.com/api/ussd/deposit/';
+                $key = 'hjASWbhdkiqa3w3n32542nNASDda';
+                $login = 'ussd_nigeria_1x';
+                $password = 'GO3zHcAeFySmyUKl';
+                $phone = $msisdn;
+                $phone_code = '234';
+
+                // $helper =  new Helpers;
+                $result = $this->callApi($url, $key, $login, $password, $phone, $phone_code, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,$amount);
+                // var_dump($result);  exit;
+                if($result){    
+                    if($result->success === true){
+                        //update status in db
+                        $this->updateTransaction1xbet($Refrence);
+                    }        
+                    return response(['response'=>$result, 200]);
+                }
+            }
+        }
+    }
+
+    public function getTransactionBet($reference){
+        // $conn  = $this->mysqliConn();
+        // $query = "SELECT * FROM bet1x_transactions WHERE 'reference' = $reference";
+        // $result = $conn -> query($query) -> fetch_assoc();
+        // var_dump($result);
+        // return (object)$result;
+    
+        $conn  = $this->pdoConn();
+        // var_dump($conn); exit;
+        $stmt = $conn->prepare("SELECT * FROM bet1x_transactions WHERE reference = ? LIMIT 1");
+        // $stmt->bindValue(":ref", $re);
+        $stmt->execute([$reference]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return (object)$result;
+    
+    }
+    
+    public function mysqliConn(){
+        $conn = new mysqli("152.228.212.181","novaji_introserve","Zh7mWr4i0A98L1mX","novaji_introserve","3306");
+    
+        // Check connection
+        if ($conn -> connect_errno) {
+            return "Failed to connect to MySQL: " . $conn -> connect_error;
+        }
+    
+        return $conn;
+    }
+    
+    public function pdoConn(){
+         $dsn = "mysql:host=152.228.212.181;dbname=novaji_introserve";
+             $dbuser = "novaji_introserve";
+             $dbpass = "Zh7mWr4i0A98L1mX";
+        try {
+            // $conn = new PDO("mysql:host=152.228.212.181;dbname=novaji_introserve", "novaji_introserve", "Zh7mWr4i0A98L1mX");
+            $conn = new PDO($dsn,$dbuser,$dbpass);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            return $conn;
+          } catch(PDOException $e) {
+            return "Connection failed: " . $e->getMessage();
+          }
+    }
+    
+    public function updateTransaction1xbet($reference)
+    {
+
+        $conn  = $this->pdoConn();
+        // var_dump($conn); exit;
+        $stmt = $conn->prepare("UPDATE bet1x_transactions SET status = 'Successful' WHERE reference = ?");
+        // $stmt->bindValue(":ref", $re);
+        $result = $stmt->execute([$reference]);
+        return $result;
+
+    }
+    
+    public function callApi($url, $key, $login, $password, $phone, $phone_code, $user_pass = NULL, $betcode = NULL, $sport_id = NULL, $page = NULL, $show_all = NULL, $is_live = NULL, $info = NULL, $match = NULL, $show_desc = NULL, $coupon = NULL, $from = NULL, $to = NULL, $amount = NULL)
+        {
+    
+            $params = [
+                'login' => $login,
+                'pass' => $password,
+                'phone' => $phone,
+                'phone_code' => $phone_code,
+            ];
+    
+            if(!empty($user_pass) || !is_null($user_pass) ){
+                $params['user_pass'] = $user_pass;
+            }
+            if(!empty($betcode) || !is_null($betcode) ){
+                $params['betcode'] = $betcode;
+            }
+            if(!empty($sport_id) || !is_null($sport_id) ){
+                $params['sport_id'] = $sport_id;
+            }
+            if(!empty($page) || !is_null($page) ){
+                $params['page'] = $page;
+            }
+            if(!empty($show_all) || !is_null($show_all) ){
+                $params['show_all'] = $show_all;
+            }
+            if(!empty($is_live) || !is_null($is_live) ){
+                $params['is_live'] = $is_live;
+            }
+            if(!empty($info) || !is_null($info) ){
+                $params['info'] = $info;
+            }
+            if(!empty($match) || !is_null($match) ){
+                $params['match'] = $match;
+            }
+            if(!empty($show_desc) || !is_null($show_desc) ){
+                $params['show_desc'] = $show_desc;
+            }
+            if(!empty($coupon) || !is_null($coupon) ){
+                $params['coupon'] = $coupon;
+            }
+    
+            if(!empty($from) || !is_null($from) ){
+                $params['from'] = $from;
+            }
+            if(!empty($to) || !is_null($to) ){
+                $params['show_desc'] = $show_desc;
+            }
+            if(!empty($amount) || !is_null($amount) ){
+                $params['amount'] = $amount;
+            }
+            
+            ksort($params);
+            $params['hash'] = base64_encode(hash_hmac('sha1', http_build_query($params), $key));
+            
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+            $result = curl_exec($ch);
+            // var_dump($result); exit;
+            $err = curl_error($ch);
+            if($err){
+                return $err;
+            }
+            curl_close($ch);
+            if ($result) {
+                $response = json_decode($result);
+                return $response;
+            }
+        }
+
+
+}
---
+++ app/Http/Controllers/GoxiController.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PDO;
+use mysqli;
+
+class GoxiController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function pdoConn(){
+        $dsn = "mysql:host=152.228.212.181;dbname=novaji_introserve";
+            $dbuser = "novaji_introserve";
+            $dbpass = "Zh7mWr4i0A98L1mX";
+       try {
+           // $conn = new PDO("mysql:host=152.228.212.181;dbname=novaji_introserve", "novaji_introserve", "Zh7mWr4i0A98L1mX");
+           $conn = new PDO($dsn,$dbuser,$dbpass);
+           // set the PDO error mode to exception
+           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+           $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+           return $conn;
+         } catch(PDOException $e) {
+           return "Connection failed: " . $e->getMessage();
+         }
+    }
+
+    public function getTransactionGoxi($reference)
+    {
+        
+        $conn  = $this->pdoConn();
+        // var_dump($conn); exit;
+        $stmt = $conn->prepare("SELECT * FROM goxi_transactions WHERE reference = ? LIMIT 1");
+        // $stmt->bindValue(":ref", $re);
+        $stmt->execute([$reference]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return (object)$result;
+
+    }
+
+    public function updateTransactionGoxi($reference)
+    { 
+
+        $conn  = $this->pdoConn();
+        // var_dump($conn); exit;
+        $stmt = $conn->prepare("UPDATE goxi_transactions SET status = 'Successful' WHERE reference = ?");
+        // $stmt->bindValue(":ref", $re);
+        $result = $stmt->execute([$reference]);
+        return $result;
+    }
+
+    public function soapApi($endpoint, $string){
+        
+        $location = $url = $endpoint;
+    
+        $soapUrl = $location;
+    
+        $xml_post_string = $string; 
+                
+                
+        $headers = array(
+            'Content-type: text/xml;charset="utf-8"',
+            'Accept: text/xml',
+            'Cache-Control: no-cache',
+            'Pragma: no-cache',
+            'Content-length: ' . strlen($xml_post_string),
+        );
+
+        $url = $soapUrl;
+
+
+
+        $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        
+        // curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_post_string); // the SOAP request
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        // converting
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+
+        $response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $response);
+        $xmlObject = simplexml_load_string($response);
+        
+
+        return $xmlObject;      
+    
+
+    }
+
+    public function createCustomer($apikey,$value, $endpoint){
+
+        $string = '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+        <CreateSinglePolicy xmlns="http://tempuri.org/">
+            <Apikey>'.$apikey.'</Apikey>
+            <Reference_Num>'.$value['ref'].'</Reference_Num>
+            <TransDate>'.$value['transdate'].'</TransDate>
+            <ProductID>'.$value['product_id'].'</ProductID>
+            <ProductName>'.$value['product_name'].'</ProductName>
+            <ClientID>'.$value['clientid'].'</ClientID>
+            <Surname>'.$value['surname'].'</Surname>
+            <OtherNames>'.$value['othername'].'</OtherNames>
+            <Address>'.$value['address'].'</Address>
+            <State>'.$value['state'].'</State>
+            <MobilePhone>'.$value['phonenumber'].'</MobilePhone>
+            <Email>'.$value['email'].'</Email>
+            <sex>'.$value['sex'].'</sex>
+            <MaritalStatus>'.$value['maritalStatus'].'</MaritalStatus>
+            <AgencyID>'.$value['agentid'].'</AgencyID>
+            <AgencyName>'.$value['agentname'].'</AgencyName>
+            <StartDate>'.$value['transdate'].'</StartDate>
+            <MaturityDate>'.$value['maturitydate'].'</MaturityDate>
+            <frequencyofPayment>'.$value['frequency'].'</frequencyofPayment>
+            <DateofBirth>'.$value['dob'].'</DateofBirth>
+            <AssuredValue>'.$value['assuredvalue'].'</AssuredValue>
+            <AmountPayable>'.$value['amount'].'</AmountPayable>
+        </CreateSinglePolicy>
+        </soap:Body>
+    </soap:Envelope>';
+        $result = $this->soapApi($endpoint, $string);
+        return $result;
+    }
+
+    public function policyPayment($apikey,$value, $endpoint){
+    
+        $string = '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+        <PolicyPayments xmlns="http://tempuri.org/">
+            <Apikey>'.$apikey.'</Apikey>
+            <DOc_Num>'.$value['docno'].'</DOc_Num>
+            <PolicyNo>'.$value['policyno'].'</PolicyNo>
+            <InsSurname>'.$value['lastname'].'</InsSurname>
+            <InsFirstname>'.$value['firstname'].'</InsFirstname>
+            <TransDate>'.$value['transdate'].'</TransDate>
+            <CoverCode>'.$value['covercode'].'</CoverCode>
+            <BrokerName>'.$value['brokername'].'</BrokerName>
+            <InsAddress>'.$value['address'].'</InsAddress>
+            <InsMobilePhone>'.$value['phone'].'</InsMobilePhone>
+            <InsEmail>'.$value['email'].'</InsEmail>
+            <StartDate>'.$value['transdate'].'</StartDate>
+            <AmountPaid>'.$value['amount'].'</AmountPaid>
+        </PolicyPayments>
+        </soap:Body>
+    </soap:Envelope>';
+        $result = $this->soapApi($endpoint, $string);
+        return $result;
+    }
+
+    public function makedeposit(){
+        $body = json_decode(@file_get_contents("php://input"));
+            // if(isset($_POST['ResponseCode']) && $_POST['Message'] && $_POST['Amount'] && $_POST['TraceId'] && $_POST['PhoneNumber'] && $_POST['Refrence'] && $_POST['InstitutionCode'] && $_POST['TransactionId'] && $_POST['TraceId']){
+            if($body){
+                $ResponseCode = $body->ResponseCode;
+                $Message = $body->Message;
+                $Amount = $body->Amount;
+                $TraceId = $body->TraceId;
+                $PhoneNumber = $body->PhoneNumber;
+                $Refrence = $body->Refrence;
+                $InstitutionCode = $body->InstitutionCode;
+                $TransactionId = $body->TransactionId;
+                $TraceId = $body->TraceId;
+                $dummy = "";
+
+                $getUser = $this->getTransactionGoxi($Refrence);
+                
+                if($getUser){
+
+                    $apikey = "F62FEED2-548F-4EC5-909F-3CAAD8039F03";
+                    $endpoint = "http://goxilife.gibsonline.com/webservice/gibs.asmx";
+
+                    //Api to register customers
+                    $value['ref'] = rand(1000000000,9999999999);
+                    $value['transdate'] = date('Y-m-d');
+                    $value['product_id'] =  $getUser->covercode;
+                    $value['product_name'] =  $getUser->product_name;
+                    $value['clientid'] = $getUser->msisdn;
+                    $value['surname'] = "";
+                    $value['othername'] = "";
+                    $value['address'] = "";
+                    $value['state'] = "";
+                    $value['phonenumber'] = $getUser->msisdn;
+                    $value['email'] = "";
+                    $value['sex'] = "";
+                    $value['maritalStatus'] = "";
+                    $value['maturitydate'] = date('Y-m-d',strtotime(date("Y-m-d", time()) . " + 365 day"));
+                    $value['frequency'] = "monthly";
+                    $value['dob'] = "";
+                    $value['amount'] = $getUser->amount;
+                    $value['assuredvalue'] = $getUser->assured;
+                    $value['agentid'] = $getUser->agentid;
+                    $value['agentname'] = $getUser->agentname;
+
+                    
+                    $productList = $this->createCustomer($apikey,$value,$endpoint);
+
+                    if($productList->soapBody->CreateSinglePolicyResponse->CreateSinglePolicyResult){
+                        $result = $productList->soapBody->CreateSinglePolicyResponse->CreateSinglePolicyResult;
+                        $number_array = explode('for ', $result);
+                        $policyno = $number_array[1]; 
+                    }
+
+
+                    //pay for policy
+                    $value = array(
+                        "docno"=>$getUser->docno,
+                        "policyno"=>$policyno,
+                        "lastname"=>$getUser->lastname,
+                        "firstname"=>$getUser->firstname,
+                        "transdate"=>$getUser->transdate,
+                        "covercode"=>$getUser->covercode,
+                        "address"=>$getUser->address,
+                        "phone"=>$getUser->phone,
+                        "email"=>$getUser->email,
+                        "amount"=>$getUser->amount,
+                        "brokername"=>$getUser->agentname
+                    );
+                    // var_dump($value); exit;
+                    // $helper =  new Helpers;
+                    $results = $this->policyPayment($apikey,$value, $endpoint);
+                    // var_dump($results); exit;
+                    if($results){
+                        $this->updateTransactionGoxi($Refrence);
+                        return response(['response'=>$results, 200]);
+                    }
+                
+                    
+                }else{
+                    return response(['response'=>"Some error occurred", 200]);
+                }
+
+                
+            }
+    }
+
+
+
+
+}
