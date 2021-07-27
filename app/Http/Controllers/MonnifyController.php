895cd8528f168b4cd7a2c5720fac2b5fb15d89f9
System Administrator <root@Users-MacBook-Pro.local>
2021-07-27 14:44:34 -0700

    goxi and 1x

--- app/Http/Controllers/MonnifyController.php
+++ app/Http/Controllers/MonnifyController.php
@@ -62,14 +62,16 @@ class MonnifyController extends Controller
             $type = $body->type;
             $merchant = $body->merchant;
             $user = $this->getUsers($phone);
+
+            
             if(isset($user)){
                 if(isset($user->reference)){
                     $accountReference = $user->reference;
                 }
-                $accountReference = rand(10000000000000, 99999999999999);;
+                $accountReference = rand(10000000000000, 99999999999999);
+                
             }
             
-
             if($type == "invoice"){
                 return $this->topup($bank_name, $amount, $phone, $customer_name, $apikey, $secretkey, $contractcode);
             }elseif($type == "reserve"){
@@ -349,7 +351,7 @@ class MonnifyController extends Controller
         }
         curl_close($handle);
     
-        return $result;
+        return response(['data'=>$result]);
 
     }
 
